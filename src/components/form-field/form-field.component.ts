4d499f9b828174d941f510f0b9da7715f5d1c0ef
Marcus Pendleton <marcuspendleton100@gmail.com>
2026-07-09 10:21:40 -0700

    thing

--- src/components/form-field/form-field.component.ts
+++ src/components/form-field/form-field.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-form-field',
   standalone: true,
-  templateUrl: './form-field.component.html',
-  styleUrl: './form-field.component.css'
+  templateUrl: './form-field.component.html'
 })
 export class FormFieldComponent {}
